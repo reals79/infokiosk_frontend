5434a2e0caae05d4e9aaf56b41f3d13293c91eb7
Анатолий Фащевский <reals@iRealsMac.local>
2020-11-15 15:21:17 +0200

    small fix

--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
-import Vue from "vue";
-import "./plugins/axios";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+'use strict'
+
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 import Buefy from 'buefy'
-import './assets/scss/app.scss';
-import VueTouchKeyboard from "vue-touch-keyboard";
-import "vue-touch-keyboard/dist/vue-touch-keyboard.css"; // load default style
+import './assets/scss/app.scss'
+import VueTouchKeyboard from 'vue-touch-keyboard'
+import 'vue-touch-keyboard/dist/vue-touch-keyboard.css' // load default style
 
-const _ = require("lodash");
-Object.defineProperty(Vue.prototype, "$_", { value: _ });
+const _ = require('lodash')
+Object.defineProperty(Vue.prototype, '$_', { value: _ })
 
-Vue.use(VueTouchKeyboard);
-Vue.use(Buefy);
+Vue.use(VueTouchKeyboard)
+Vue.use(Buefy)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app')
